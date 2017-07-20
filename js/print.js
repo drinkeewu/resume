c1210c9bbf101d84330fe0cc6a62cfcd662759ad
drinkeewu <773119690@qq.com>
2017-07-20 12:22:34 +0800

    first version

---
+++ js/print.js
@@ -0,0 +1,12 @@
+/*
+* @Author: drinkeewu
+* @Date:   2017-07-20 09:46:13
+* @Last Modified by:   drinkeewu
+* @Last Modified time: 2017-07-20 10:17:17
+*/
+
+(function(){
+	document.getElementsByClassName('print-btn')[0].addEventListener('click', function(){
+		window.print();
+	});
+})();

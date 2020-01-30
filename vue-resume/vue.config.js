ca4ed9b61c4fe79f6f2487d789a2b7ece4f1b2f9
drinkeewu <773119690@qq.com>
2020-01-30 22:27:14 +0800

    chore:setup the build config

--- vue-resume/vue.config.js
+++ vue-resume/vue.config.js
@@ -1,9 +1,5 @@
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'development'
-    ? ''
-    : '/resume/page',
-  outputDir: '../page',
   devServer: {
     overlay: {
       warnings: false,

c6ad9247c5ca00df4bcfc509c94d072a8dc97084
drinkeewu <773119690@qq.com>
2020-02-07 13:56:09 +0800

    chore: 删除没有被引用的组件

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,5 @@ export { default as Flex } from './flex';
 export { default as FixButton } from './fix-button';
 export { default as TextSection } from './text-section';
 export { default as BlockWrapper } from './block-wrapper';
-export { default as Header } from './header';
 export { default as ProgressList } from './progress';
 export { default as IconLineList } from './icon-line';
